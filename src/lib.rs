07ac663c2932596ded2cbe926e4304eb959d7315
Elichai Turkel <elichai.turkel@gmail.com>
2019-05-02 14:13:54 +0300

    Added more tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,21 @@
 //!
 //! ## Examples
 //! ```rust
-//!     use stdio_override::StdoutOverride;
-//!     use std::{fs, mem};
-//!     let file_name = "./test.txt";
-//!     let guard = StdoutOverride::override_file(file_name).unwrap();
-//!      println!("Isan to Stdout!");
+//!# fn main() -> std::io:: Result<()> {
+//! use stdio_override::StdoutOverride;
+//! use std::{fs, mem};
+//! let file_name = "./test.txt";
+//!# std::fs::remove_file(file_name);
+//! let guard = StdoutOverride::override_file(file_name)?;
+//! println!("Isan to Stdout!");
 //!
-//!     let contents = fs::read_to_string(file_name).unwrap();
-//!     assert_eq!("Isan to Stdout!\n", contents);
-//!     mem::drop(guard);
-//!     println!("Outside!");
-
+//! let contents = fs::read_to_string(file_name)?;
+//! assert_eq!("Isan to Stdout!\n", contents);
+//! mem::drop(guard);
+//! println!("Outside!");
+//!
+//!# Ok(())
+//!# }
 //! ```
 //!
 mod ffi;
@@ -37,9 +41,110 @@ fd_guard!(StdinOverride, guard: StdinOverrideGuard, FD: crate::ffi::STDIN_FILENO
 fd_guard!(StderrOverride, guard: StderrOverrideGuard, FD: crate::ffi::STDERR_FILENO, name of FD: stderr);
 
 pub use crate::stderr::{StderrOverride, StderrOverrideGuard};
-
-
+pub use crate::stdin::{StdinOverride, StdinOverrideGuard};
 pub use crate::stdout::{StdoutOverride, StdoutOverrideGuard};
 
 #[cfg(feature = "test-readme")]
 doc_comment::doctest!("../README.md");
+
+#[cfg(test)]
+mod test {
+    use crate::ffi::*;
+    use crate::*;
+    use std::os::unix::io::IntoRawFd;
+    use std::{
+        fs::{read_to_string, File},
+        io::{stdin, stdout, Read, Result, Write},
+        mem,
+    };
+    use tempfile;
+
+    fn get_new_file_path() -> Result<tempfile::TempPath> {
+        Ok(tempfile::NamedTempFile::new()?.into_temp_path())
+    }
+
+    #[test]
+    fn test_stdout() -> Result<()> {
+        let file_path = get_new_file_path()?;
+        let data = "12345";
+
+        let guard = StdoutOverride::override_file(&file_path)?;
+        print!("{}", data);
+        stdout().flush()?;
+        mem::drop(guard);
+
+        let contents = read_to_string(file_path)?;
+        assert_eq!(data, contents);
+        println!("Outside!");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_stderr() -> Result<()> {
+        let file_path = get_new_file_path()?;
+        let data = "123456";
+
+        let guard = StderrOverride::override_file(&file_path)?;
+        eprint!("{}", data);
+        stdout().flush()?;
+        mem::drop(guard);
+
+        let contents = read_to_string(file_path)?;
+        assert_eq!(data, contents);
+        println!("Outside!");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_stdin() -> Result<()> {
+        let file_path = get_new_file_path()?;
+        let data = "12345";
+        {
+            let mut file = File::create(&file_path)?;
+            file.write_all(data.as_bytes())?;
+        }
+
+        let guard = StdinOverride::override_file(&file_path)?;
+
+        let mut s = String::new();
+        print!("Please enter some text: ");
+        stdout().flush()?;
+        stdin().read_line(&mut s)?;
+
+        mem::drop(guard);
+
+        let contents = read_to_string(file_path)?;
+        assert_eq!(data, contents);
+
+        println!("You typed: {}", s);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_original() -> Result<()> {
+        let file_path = get_new_file_path()?;
+
+        let file = File::create(&file_path)?;
+        let file = file.into_raw_fd();
+
+        let real_stdout = unsafe { dup(STDOUT_FILENO) }?;
+
+        unsafe { dup2(file, STDOUT_FILENO) }?;
+
+        println!("Let's see where it's saved");
+        let mut file = File::open(&file_path)?;
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        stdout().lock().flush()?;
+        unsafe { dup2(real_stdout, STDOUT_FILENO) }?;
+        assert_eq!("Let\'s see where it\'s saved\n", contents);
+
+        println!("got back");
+
+        Ok(())
+    }
+}
